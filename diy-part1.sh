2e8819ba28a0fbd57e471265f25542e0eecc6b42
lllrrr <6911742@qq.com>
2020-06-14 15:13:47 +0800

    init

---
+++ diy-part1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/luci.git^fb2f36306756d0d0782dcab8413a8bb7ec379e54/luci.git;openwrt-18.06/g' feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
