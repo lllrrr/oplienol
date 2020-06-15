df04e19e0407664002848ba008cec0bdca49cb90
lllrrr <6911742@qq.com>
2020-06-15 19:03:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,13 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark-mod/g' feeds/luci/collectio
 #sed -i 's/+kmod-nls-base @BUILD_PATENTED/+kmod-nls-base/g' feeds/packages/kernel/exfat-nofuse/Makefile
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=R9.3-$(shell date +%F-%H)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
-rm -rf package/default-settings/
-rm -rf package/diy/
-rm -rf package/fullconenat/
-rm -rf package/lean/
-rm -rf package/OpenAppFilter/
-rm -rf feeds/luci/applications/luci-app-smartdns/
-rm -rf feeds/packages/net/smartdns/
+rm -rf package/default-settings
+rm -rf package/diy
+rm -rf package/fullconenat
+rm -rf package/lean
+rm -rf package/OpenAppFilter
+rm -rf feeds/luci/applications/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun package/lean/ddns-scripts_aliyun
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_dnspod package/lean/ddns-scripts_dnspod
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings package/lean/default-settings
